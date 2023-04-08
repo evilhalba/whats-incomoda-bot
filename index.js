39e0fc7d6eca2bbd668f72ecae4bfbea112cff04
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-04-07 23:23:17 -0300

    Start Project

--- index.js
+++ index.js
@@ -56,8 +56,7 @@ venom
   } 
 
   const greetingMessage = () => {
-    //let h = new Date().toLocaleTimeString('pt-BR', { hour: 'numeric', hour12: false });
-    let h = new Date().getHours();
+    let h = new Date().toLocaleTimeString('pt-BR', { hour: 'numeric', hour12: false });
     if (h <= 5) return 'Boa madrugada';
     if (h < 12) return 'Bom dia';
     if (h < 18) return 'Boa tarde';
